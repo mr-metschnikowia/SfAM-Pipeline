ec857453e72a664299d8b190e037fc07b7893f29
mr-metschnikowia <67706430+mr-metschnikowia@users.noreply.github.com>
2020-08-28 16:43:51 +0100

    Update R_Script1.R

--- R_Script1.R
+++ R_Script1.R
@@ -4,16 +4,16 @@ library(ggplot2)
 library(RColorBrewer)
 # importing prerequisites
 
-df <- fread('C:/Users/Rhino/blast+/Outputs/output.txt',select=c(2,5))
+df <- fread('/home/centos/blast+/outputs/output.txt',select=c(2,5))
 # importing relevant columns from .csv table of blast output
 krusk <- kruskal.test(df$qcovs,df$staxid)
 chars <- capture.output(print(krusk))
-writeLines(chars, con = file("C:/Users/Rhino/PycharmProjects/sfam/Figures/Krusk.txt"))
+writeLines(chars, con = file("/home/centos/project/outputs/Krusk.txt"))
 # data is not normally distributed and not matched pairs
 # Kruskal-Wallis test is used to explore variation in qcovs between strains
 # output is saved as .txt file
 unstacked.qcovs = unstack(df[,c(2,1)])
-png('C:/Users/Rhino/PycharmProjects/sfam/Figures/boxplot.png')
+png('/home/centos/project/outputs/boxplot.png')
 boxplot(unstacked.qcovs,col='yellow')
 mtext('Strain Index', side=1, line=3, col='blue')
 mtext('Query Coverage', side=2, line=3, col='blue', las=0)
@@ -21,13 +21,14 @@ title(main='Box and whisker plot of query coverage across yeast strains', col='b
 dev.off()
 # boxplot is created (in .png file) to visualise data
 
-df2 <- fread('C:/Users/Rhino/PycharmProjects/sfam/pandas_dfs/df1.csv',select=c(3,4,9))
+df2 <- fread('/home/centos/project/dfs/df1.csv',select=c(3,4,9))
 # importing relevant columns from .csv table of blast output
 df3 <- ddply(df2,~staxid+clusters,summarise,accession_count=length(unique(sacc)))
 df3$staxid <- as.factor(df3$staxid)
 # creating new dataframe: number of unique accesion codes counted, aggregated on tax id and cluster
-png('C:/Users/Rhino/PycharmProjects/sfam/Figures/barchart.png')
-myColors <- brewer.pal(5, "Set3")
+png('/home/centos/project/outputs/barchart.png')
+no_colours = length(unique(df3$clusters))
+myColors <- brewer.pal(no_colours, "Set3")
 names(myColors) <- length(unique(df3$sacc))
 bar <- ggplot(df3, aes(x=clusters, y=accession_count, fill=staxid)) + geom_bar(stat="identity") + scale_colour_manual(values=myColors)
 bar <- bar + ggtitle("Distribution of gene clusters across accessions and across strains") + theme(plot.title=element_text(face="bold"))
@@ -35,5 +36,4 @@ bar <- bar + scale_fill_discrete(name="Strain\nIndex")
 bar <- bar + xlab("Cluster") + ylab("Accession Count")
 bar
 dev.off()
-# barchart is created (in .png file) for data visualisation
-
+# barchart is created (in .png file) to visualise data
