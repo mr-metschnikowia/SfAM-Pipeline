025cbb5b8c8543156a4b42121e5196b9b47bc43a
mr-metschnikowia <67706430+mr-metschnikowia@users.noreply.github.com>
2020-09-02 20:03:06 +0100

    Update R_Script1.R

--- R_Script1.R
+++ R_Script1.R
@@ -21,7 +21,7 @@ title(main='Box and whisker plot of query coverage across yeast strains', col='b
 dev.off()
 # boxplot is created (in .png file) to visualise data
 
-df2 <- fread('/home/centos/project/dfs/df1.csv',select=c(3,4,9))
+df2 <- fread('/home/centos/project/dfs/df1.csv',select=c(3,4,12))
 # importing relevant columns from .csv table of blast output
 df3 <- ddply(df2,~staxid+clusters,summarise,accession_count=length(unique(sacc)))
 df3$staxid <- as.factor(df3$staxid)
@@ -37,3 +37,17 @@ bar <- bar + xlab("Cluster") + ylab("Accession Count")
 bar
 dev.off()
 # barchart is created (in .png file) to visualise data
+
+df4 <- fread('/home/centos/project/dfs/df1.csv',select=c(2,3,11))
+png('/home/centos/project/outputs/hist2.png')
+hist(df4$`dnds`,xlab='dn/ds',main='Distribution of dn/ds')
+dev.off()
+png('/home/centos/project/outputs/barchart2.png')
+no_colours = length(unique(df4$qacc))
+myColors <- brewer.pal(no_colours, "Set3")
+bar2 <- ggplot(df4, aes(x=staxid, y=dnds, fill=qacc)) + geom_bar(stat="identity") + scale_colour_manual(values=myColors)
+bar2 <- bar2 + ggtitle("Distribution of dn/ds across strains and across homologs") + theme(plot.title=element_text(face="bold"))
+bar2 <- bar2 + scale_fill_discrete(name="Gene")
+bar2 <- bar2 + xlab("Strain") + ylab("dn/ds")
+bar2
+dev.off()
