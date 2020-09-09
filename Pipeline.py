6fa0acabf4760c2787cb4fc4b0a6dcc2da625d91
mr-metschnikowia <67706430+mr-metschnikowia@users.noreply.github.com>
2020-09-10 00:35:32 +0100

    Update Pipeline.py

--- Pipeline.py
+++ Pipeline.py
@@ -60,74 +60,82 @@ def hist_me_up(output):
     # empty data frame is accomodated for
 
 def clusterize():
+    global prediction
     column = df.qcovs
     array = column.values
-    prediction = int(input('Please predict number of clusters:'))
+    # prediction = int(input('Please predict number of clusters:'))
+    prediction = 5
     compatible = array.reshape(-1,1)
     km = KMeans(n_clusters=prediction,random_state=0).fit(compatible)
     labels = km.labels_
     labels = [str(i) for i in labels]
     df['clusters'] = labels
+    df.to_csv(r'/home/centos/project/dfs/reference.csv')
     # 1D array created from dataframe column of interest
     # 2D array created for compatibility
     # k-means cluster analysis is performed on 2D array
     # cluster data is added to dataframe
+    # dataframe is shipped as .csv to provide reference for R script
 
 def DNDS():
-    column = []
-    for i in range(len(df.qseq)):
-        codons = ['-', 'K', 'M', 'B', 'V', 'S', 'W', 'D', 'Y', 'R', 'H']
-        for codon in codons:
-            while df.qseq[i].find(codon) > -1:
-                df.sseq[i] = df.sseq[i][0: df.qseq[i].find(codon)] + df.sseq[i][df.qseq[i].find(codon) + 1:]
-                df.qseq[i] = df.qseq[i][0: df.qseq[i].find(codon)] + df.qseq[i][df.qseq[i].find(codon) + 1:]
-            while df.sseq[i].find(codon) > -1:
-                df.qseq[i] = df.qseq[i][0: df.sseq[i].find(codon)] + df.qseq[i][df.sseq[i].find(codon) + 1:]
-                df.sseq[i] = df.sseq[i][0: df.sseq[i].find(codon)] + df.sseq[i][df.sseq[i].find(codon) + 1:]
-        # removing gaps
-        while len(df.qseq[i]) % 3 > 0:
-            df.qseq[i] = df.qseq[i][:-1]
-            df.sseq[i] = df.sseq[i][:-1]
-        # trimming sequences that are not multiples of 3
-        seq_1 = df.qseq[i]
-        seq_2 = df.sseq[i]
-        try:
-            x = round(dnds(seq_1, seq_2), 3)
-        except ZeroDivisionError:
-            x = 0
-        column.append(x)
-        # dnds calculated for each hit relative to APC1.2
-    df['dnds'] = column
-    # dnds column added to dataframe
-    # x = 0
-    # # y = 3
-    # # seq = 'AAABBBCCCDD-'
-    # # for i in range(int(len(seq)/3)):
-    # #     if seq[x:y].find('-') > -1:
-    # #         seq = seq[0:x] + seq[y:]
-    # #     x = y
-    # #     y = x + 3
-    # # # alternative method for omitting unwanted codons, taking triplets into account
+    global target_frame2
+    for j in range(len(target_frame2.sseq)):
+        column = []
+        for i in range(len(target_frame2.sseq)):
+            reference = target_frame2.sseq[j]
+            reference_length = len(reference)
+            seq2 = target_frame2.sseq[i]
+            seq2_length = len(seq2)
+            if seq2_length > reference_length:
+                seq2 = seq2[:reference_length]
+            else:
+                reference = reference[:seq2_length]
+            # ensuring equal length
+            for c in range(reference_length):
+                codons = ['-', 'K', 'M', 'B', 'V', 'S', 'W', 'D', 'Y', 'R', 'H']
+                for codon in codons:
+                    while reference.find(codon) > -1:
+                        seq2 = seq2[0: reference.find(codon)] + seq2[reference.find(codon) + 1:]
+                        reference = reference[0: reference.find(codon)] + reference[reference.find(codon) + 1:]
+                    while seq2.find(codon) > -1:
+                        reference = reference[0: seq2.find(codon)] + reference[seq2.find(codon) + 1:]
+                        seq2 = seq2[0: seq2.find(codon)] + seq2[seq2.find(codon) + 1:]
+                # removing ambiguous codons
+            while len(reference) % 3 > 0:
+                reference = reference[:-1]
+                seq2 = seq2[:-1]
+            # trimming sequences that are not multiples of 3
+            try:
+                x = round(dnds(reference, seq2), 3)
+            except ZeroDivisionError:
+                x = 0
+            except ValueError:
+                x = 'math_error'
+            column.append(x)
+            # dnds calculated for each hit relative to all other hits
+        column_name = str(target_frame2.staxid[j]) + '-{}'.format(target_frame2.sacc[j])
+        target_frame2[column_name] = column
+        # dnds column for each hit added to dataframe
 
 def intergenic_space_count():
-    global df
-    df.sort_values(['staxid', 'sacc', 'sstart'], inplace=True)
-    df = df.reset_index(drop=True)
+    global ready_df
+    ready_df.sort_values(['staxid', 'sacc', 'sstart'], inplace=True)
+    ready_df = ready_df.reset_index(drop=True)
     # data ordered by staxid, sacc and sstart
     total_intergenic_space = []
-    for i in range(len(df.staxid)):
-        if df.sstart[i] > df.send[i]:
-            x = df.sstart[i]
-            y = df.send[i]
-            df.sstart[i] = y
-            df.send[i] = x
+    for i in range(len(ready_df.staxid)):
+        if ready_df.sstart[i] > ready_df.send[i]:
+            x = ready_df.sstart[i]
+            y = ready_df.send[i]
+            ready_df.sstart[i] = y
+            ready_df.send[i] = x
     # sstart and ssend are flipped if ssend < sstart for standardisation purposes
-    for i in range(len(df.staxid) - 1):
-        staxid = df.staxid[i]
-        sacc = df.sacc[i]
-        if staxid == df.staxid[i + 1]:
-            if sacc == df.sacc[i + 1]:
-                intergenic_space = df.sstart[i + 1] - df.send[i]
+    for i in range(len(ready_df.staxid) - 1):
+        staxid = ready_df.staxid[i]
+        sacc = ready_df.sacc[i]
+        if staxid == ready_df.staxid[i + 1]:
+            if sacc == ready_df.sacc[i + 1]:
+                intergenic_space = ready_df.sstart[i + 1] - ready_df.send[i]
                 total_intergenic_space.append(intergenic_space)
             else:
                 intergenic_space = 0
@@ -136,8 +144,25 @@ def intergenic_space_count():
             intergenic_space = 0
             total_intergenic_space.append(intergenic_space)
     total_intergenic_space.append(0)
-    df['intergenic_space'] = total_intergenic_space
+    ready_df['intergenic_space'] = total_intergenic_space
     # intergenic space between each gene on same chromosme/contig in same strain is calculated
+
+def GenePair():
+    global ready_df
+    possible_pairs = ['pul1-pul1','pul1-pul2','pul1-pul3','pul1-pul4','pul2-pul2','pul2-pul3','pul2-pul4','pul3-pul3','pul3-pul4','pul4-pul4']
+    column = []
+    for i in range(len(ready_df.intergenic_space)):
+        if ready_df.intergenic_space[i] > 0:
+            gene_pair = '{}-{}'.format(ready_df.qacc[i],ready_df.qacc[i+1])
+            if gene_pair not in possible_pairs:
+                gene_pair = '{}-{}'.format(ready_df.qacc[i+1],ready_df.qacc[i])
+                column.append(gene_pair)
+            else:
+                column.append(gene_pair)
+        else:
+            column.append('NA')
+    ready_df['gene_pair'] = column
+    # for each intergenic sequence standardised gene pair is allocated
 
 def extract_intergenic_seqs():
     with open(r'/home/centos/project/genomes/custom.txt', 'r') as f:
@@ -220,29 +245,88 @@ def R_script(script):
     # R script is run
 
 if __name__ == '__main__':
-    batch_q()
-    os.system('python3 /home/centos/project/code/taxmap_generator.py')
-    # generating taxonomic map
-    pre_db()
-    make_blast_db('custom.txt','custom_db','tax_map1.txt')
-    batch_blastn('batch.txt','custom_db','output.txt')
+    # batch_q()
+    # os.system('python3 /home/centos/project/code/taxmap_generator.py')
+    # # generating taxonomic map
+    # pre_db()
+    # make_blast_db('custom.txt','custom_db','tax_map1.txt')
+    # batch_blastn('batch.txt','custom_db','output.txt')
     dataframe(r'/home/centos/blast+/outputs/output.txt')
-    hist_me_up(r'/home/centos/project/outputs/histogram.png')
+    # hist_me_up(r'/home/centos/project/outputs/histogram.png')
     clusterize()
-    DNDS()
-    intergenic_space_count()
-    extract_intergenic_seqs()
-    account_for_Ns()
-    df.to_csv(r'/home/centos/project/dfs/df1.csv')
+    genes = ['pul1', 'pul2', 'pul3', 'pul4']
+    grouped = df.groupby(df.qacc)
+    pul1 = grouped.get_group("pul1")
+    pul2 = grouped.get_group("pul2")
+    pul3 = grouped.get_group("pul3")
+    pul4 = grouped.get_group("pul4")
+    pul1.to_csv(r'/home/centos/project/dfs/pul1.csv')
+    pul2.to_csv(r'/home/centos/project/dfs/pul2.csv')
+    pul3.to_csv(r'/home/centos/project/dfs/pul3.csv')
+    pul4.to_csv(r'/home/centos/project/dfs/pul4.csv')
+    # master df split by gene
+    cluster_df_names = []
+    for gene in genes:
+        target_frame = locals()[gene]
+        grouped = target_frame.groupby(target_frame.clusters)
+        names_of_unique_clusters = target_frame.clusters.unique()
+        unwanted = ['qacc', 'pident', 'qcovs', 'sstart', 'send', 'qseq', 'clusters']
+        for item in unwanted:
+            del target_frame[item]
+        # unwanted columns are ditched
+        for name in names_of_unique_clusters:
+            df_name = gene + '_' + 'cluster' + name
+            cluster_df_names.append(df_name)
+            locals()[df_name] = grouped.get_group(name)
+            # make string into variable name
+            # gene dataframes split by cluster
+    count = 0
+    for name in cluster_df_names:
+        target_frame2 = locals()[name]
+        target_frame2 = target_frame2.reset_index(drop=True)
+        if len(target_frame2.staxid) > 1:
+            DNDS()
+            target_frame2.to_csv(r'/home/centos/project/dfs/dnds_{}.csv'.format(name))
+        else:
+            with open(r'/home/centos/project/dfs/error_statement{}.txt'.format(count), 'w') as f:
+                f.write('This({}) dataframe has only has one entry'.format(name))
+                count += 1
+    # DNDS calculated for each dataframe split on gene and cluster
+    clusters = []
+    for i in range(prediction):
+        clusters.append(str(i))
+    grouped = df.groupby(df.clusters)
+    cluster_df_names2 = []
+    for cluster in clusters:
+        name = 'cluster{}'.format(cluster)
+        cluster_df_names2.append(name)
+        locals()[name] = grouped.get_group(cluster)
+    # master dataframe split on just clusters
+    for name in cluster_df_names2:
+        ready_df = locals()[name]
+        unwanted = ['pident', 'qcovs', 'qseq', 'sseq','clusters']
+        for item in unwanted:
+            del ready_df[item]
+        intergenic_space_count()
+        GenePair()
+        culprits = ready_df[ready_df['gene_pair'] == 'NA'].index
+        ready_df.drop(culprits, inplace = True)
+        if len(ready_df.qacc) > 0:
+            ready_df.to_csv(r'/home/centos/project/dfs/{}.csv'.format(name))
+    # intergenic sequence length calculated for each dataframe split by cluster
+    # each intergenic sequence is assigned a gene pair
     R_script('/home/centos/project/code/Script1.R')
-    batch_from_string()
-    database_from_string()
-    make_blast_db('custom2.txt', 'custom2', 'tax_map2.txt')
-    batch_blastn('batch2.txt', 'custom2', 'output2.txt')
-    dataframe(r'/home/centos/blast+/outputs/output2.txt')
-    hist_me_up(r'/home/centos/project/outputs/histogram3.png')
-    if empty_df == False:
-        R_script('/home/centos/project/code/Script2.R')
+    # account_for_Ns()
+    # df.to_csv(r'/home/centos/project/dfs/df1.csv')
+    # R_script('/home/centos/project/code/Script1.R')
+    # batch_from_string()
+    # database_from_string()
+    # make_blast_db('custom2.txt', 'custom2', 'tax_map2.txt')
+    # batch_blastn('batch2.txt', 'custom2', 'output2.txt')
+    # dataframe(r'/home/centos/blast+/outputs/output2.txt')
+    # hist_me_up(r'/home/centos/project/outputs/histogram3.png')
+    # if empty_df == False:
+    #     R_script('/home/centos/project/code/Script2.R')
     # calling functions
     # data frames are exported as .csv files
     # Second R script is run if data frame contains data
